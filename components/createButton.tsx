d3dd7d93db5056e62657578861949cc7fdcfa561
tanakalucky <39301079+tanakalucky@users.noreply.github.com>
2024-01-03 04:15:56 +0000

    インターセプトルーティング構成修正

--- components/createButton.tsx
+++ components/createButton.tsx
@@ -7,12 +7,7 @@ export default function CreateButton() {
   const router = useRouter();
 
   return (
-    /**
-     * @todo 動的ルーティングを併用したインターセプトルーティングを用いると
-     * 動的ルーティングを併用しないインターセプトルーティングが正常に動作しない
-     * 暫定的に適当なidを指定して新規登録モーダルを表示する
-     */
-    <Button className='bg-black text-white' onClick={() => router.push('/create-dialog/0', { scroll: false })}>
+    <Button className='bg-black text-white' onClick={() => router.push('/create-dialog', { scroll: false })}>
       Add New Task
     </Button>
   );
